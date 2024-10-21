2707db25317022ea7021d7df9d21f29539e89317
Muhammad Fadli <muhammadfadli080818@gmail.com>
2024-10-22 02:06:28 +0800

    Change Contact

--- src/components/ContactComp.jsx
+++ src/components/ContactComp.jsx
@@ -4,23 +4,14 @@ import { IoLogoWhatsapp, IoMdMail } from "react-icons/io";
 const ContactComp = () => {
   return (
     <div className=" bg-slate-50 w-11/12 md:w-2/4 border border-slate-300 h-auto rounded-lg p-2">
-      <h1 className="text-lg text-center font-semibold mb-3">
-        Contact Me On :
-      </h1>
+      <h1 className="text-lg text-center font-semibold mb-3">Contact :</h1>
       <div className="flex items-center justify-center text-sm">
-        <IoLogoWhatsapp size={25} className="mt-2 text-green-600" />
-        <p className="ml-2">
-          {" "}
-          Whatsapp : <span className="font-semibold">+62 822 1364 8742</span>
-        </p>
+        <IoLogoWhatsapp size={25} className="mt-2 text-slate-600" />
+        <p className="ml-2">+62 822 1364 8742</p>
       </div>
       <div className="flex items-center justify-center text-sm mb-4">
-        <IoMdMail size={25} className="mt-2 text-red-500" />
-        <p className="ml-2">
-          {" "}
-          E-mail :{" "}
-          <span className="font-semibold">muhammadfadli080818@gmail.com</span>
-        </p>
+        <IoMdMail size={25} className="mt-2 text-slate-600" />
+        <p className="ml-2">muhammadfadli080818@gmail.com</p>
       </div>
     </div>
   );
